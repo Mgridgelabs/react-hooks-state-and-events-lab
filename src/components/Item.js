58c235306af7258010184ed9c7ef51b32e01b921
Mgridgelabs <mainaianndungu@gmail.com>
2024-08-03 01:09:45 +0300

    complete hooks-state-events-lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [incart,setInCart] = useState(false)
+
+  const handleClick = ()=> {
+    setInCart(!incart);
+  }
   return (
-    <li className="">
+    <li className={incart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onclick={handleClick}>
+        {incart ? "Remove from cart" : "Add to cart"}
+      </button>
     </li>
   );
 }
